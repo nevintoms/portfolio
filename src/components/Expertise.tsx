3764ab4c51f89f6a65c68b25a2afa45c0025f814
Nevin Toms <nevintoms333@gmail.com>
2025-07-27 10:52:22 -0400

    Initial setup

---
+++ src/components/Expertise.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import Chip from '@mui/material/Chip';
+import '../assets/styles/Expertise.scss';
+
+const labelsFirst = [
+    "Python",
+    "TensorFlow",
+    "LlamaIndex",
+    "MLflow",
+    "FastAPI",
+    "Docker",
+    "Kubernetes",
+    "Python",
+    "SQL",
+    "PostgreSQL",
+    "Postman"
+];
+
+const labelsSecond = [
+    "Git",
+    "GitHub Actions",
+    "Docker",
+    "AWS",
+    "Azure",
+    "Linux",
+    "Snowflake",
+    "Pandas",
+    "Selenium",
+];
+
+const labelsThird = [
+    "OpenAI",
+    "Groq",
+    "LangChain",
+    "Qdrant",
+    "Hugging Face",
+    "LlamaIndex",
+    "Streamlit",
+];
+
+function Expertise() {
+    return (
+    <div className="container" id="expertise">
+        <div className="skills-container">
+            <h1>Expertise</h1>
+            <div className="skills-grid">
+                <div className="skill">
+                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                    <h3>Machine Learning</h3>
+                    <p>I develop and fine-tune machine learning models for prediction, anomaly detection, and NLP use cases. I apply statistical techniques and deep learning frameworks to extract insights and drive decisions across multiple domains.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsFirst.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="skill">
+                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <h3>GenAI, LLMs & MLOps</h3>
+                    <p>I build and deploy intelligent systems using large language models, retrieval-augmented generation (RAG), and agentic workflows. I orchestrate tools like LangChain and LlamaIndex, containerize apps using Docker, and ensure reliability through CI/CD and model monitoring.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsSecond.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="skill">
+                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <h3>Data Engineering & Pipelines</h3>
+                    <p>I design and manage scalable data pipelines for ingestion, transformation, and storage of structured and unstructured data. I work with distributed systems and cloud-native tools to support analytics and machine learning workflows.</p>
+                    <div className="flex-chips">
+                        <span className="chip-title">Tech stack:</span>
+                        {labelsThird.map((label, index) => (
+                            <Chip key={index} className='chip' label={label} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Expertise;
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../assets/styles/Timeline.scss'
+
+function Timeline() {
+  return (
+    <div id="history">
+      <div className="items-container">
+        <h1>Career History</h1>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="April 2024 - June 2025"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Steer Health</h3>
+            <h4 className="vertical-timeline-element-subtitle">Toronto, Canada</h4>
+            <p>
+              Data Engineer II
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="September 2024 - April 2025"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Centennial College</h3>
+            <h4 className="vertical-timeline-element-subtitle">Toronto, Canada</h4>
+            <p>
+              Post graduate certificate in Cybersecurity.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="November 2023 - April 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Kevares Autonomous Services</h3>
+            <h4 className="vertical-timeline-element-subtitle">Toronto, Canada</h4>
+            <p>
+              Research Engineer - Intern.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="September 2023 - April 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Humber College</h3>
+            <h4 className="vertical-timeline-element-subtitle">Toronto, Canada</h4>
+            <p>
+              Post graduate certificate in Artificial Intelligence with Machine Learning.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2023 - 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Prevalent AI</h3>
+            <h4 className="vertical-timeline-element-subtitle">Kochi, India</h4>
+            <p>
+              Data Scientist
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2022 - 2023"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Prevalent AI</h3>
+            <h4 className="vertical-timeline-element-subtitle">Kochi, India</h4>
+            <p>
+              Data Analyst
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="August 2017 - June 2021"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">APJ Abdul Kalam Technological University</h3>
+            <h4 className="vertical-timeline-element-subtitle">Kochi, India</h4>
+            <p>
+              Bachelor of Computer Science and Engineering
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
